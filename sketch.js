2499fc0268fe1deb081f69ed2b90d918cdf2a07e
awdes1846 <74605280+awdes1846@users.noreply.github.com>
2022-06-14 15:29:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var bullet,wall,thickness;
+var speed, weight; 
+
+function setup() {
+  createCanvas(1600,400);
+
+  wall=createSprite(1200, 200, thickness, 50);
+  speed=random(223,321)
+  weight=random(30,52)
+  thickness=random(22,83)
+  bullet=createSprite(50,200,20,10)
+    
+    
+    
+}
+
+function draw() {
+  background(0,0,0); 
+  
+  bullet.velocityX = speed
+
+  if(hasCollided(bullet,wall)){
+    bullet.velocityX=0;
+    var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
+
+    if(damage>10)
+    {
+      wall.shapeColor=color(255,0,0);
+    }
+
+    if(damage<10)
+    {
+      wall.shapeColor=color(0,255,0);
+    }
+  }
+  
+  drawSprites();
+}
+function hasCollided(Lbullet,Lwall){
+  bulletRightEdge=Lbullet.x+Lbullet.width;
+  wallLeftEdge=Lwall.x;
+  if(bulletRightEdge>=wallLeftEdge)
+  {
+    return true
+  }
+  return false;
+}
